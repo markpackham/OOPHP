3855bb48936e1106865b684dde7e5ca82d7da23b
Mark Packham <markpackham1@gmail.com>
2019-12-18 18:55:35 +0000

    Revise OO PHP from https://www.youtube.com/watch?v=0OOAdIdQvX4&list=PL4cUxeGkcC9hNpT-yVAYxNWOmxjxL51Hy&index=3

---
+++ ooRefresher.php
@@ -0,0 +1,26 @@
+<?php
+
+class User
+{
+
+}
+
+$userOne = new User();
+$userTwo = new User();
+
+echo 'the class is ' . get_class($userOne);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Object Oriented PHP</title>
+</head>
+<body>
+
+</body>
+</html>
