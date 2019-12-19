398b889d2719c01ea699357c8725e80012faa110
Mark Packham <markpackham1@gmail.com>
2019-12-19 19:31:55 +0000

    revise Protected status https://www.youtube.com/watch?v=wmc9cF3w0xg&list=PL4cUxeGkcC9hNpT-yVAYxNWOmxjxL51Hy&index=9

--- ooRefresher.php
+++ ooRefresher.php
@@ -2,7 +2,8 @@
 class User
 {
     public $username;
-    private $email;
+    protected $email;
+    public $role = 'member';
     public function __construct($username, $email)
     {
         //$this->username = 'ken';
@@ -14,6 +15,10 @@ class User
         //return "added a new friend";
         return "$this->username just added a new friend";
     }
+    public function message()
+    {
+        return "$this->email sent a new message";
+    }
     // getters
     public function getEmail()
     {
@@ -22,21 +27,30 @@ class User
     // setters
     public function setEmail($username)
     {
-        // check that an @ symbol exists to prove it is an email
-        // via string position
         if (strpos($username, '@') > -1) {
             $this->email = $username;
         };
     }
 }
+class AdminUser extends User
+{
+    public $level;
+    public $role = 'admin';
+    public function __construct($username, $email, $level)
+    {
+        parent::__construct($username, $email);
+        $this->level = $level;
+    }
+    public function message()
+    {
+        return "admin $this->email sent a new message";
+    }
+}
 $userOne = new User('mario', 'mario@thenetninja.co.uk');
 $userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
-echo $userOne->getEmail() . '<br>';
-echo $userTwo->getEmail() . '<br>';
-
-$userTwo->setEmail('yoshi@thenetninja.co.uk');
-
-echo $userTwo->getEmail() . '<br>';
+$userThree = new AdminUser('yoshi', 'yoshi@thenetninja.co.uk', 5);
+echo $userOne->message() . '<br>';
+echo $userThree->message() . '<br>';
 ?>
 
 <!DOCTYPE html>
